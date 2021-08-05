5db71948143c819cabdf05944852895112d73ee9
Yunsu <kenatman0723@gmail.com>
2021-08-05 14:38:18 +0900

    small practice

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log(`hello`);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = `YS`, age = 32, gender = `male`;
+const sayHello = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age} years old, also you are ${gender}`);
+};
+sayHello(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log(`hello`);
+const name = `YS`,
+  age = 32,
+  gender = `male`;
+
+const sayHello = (name, age, gender?) => {
+  console.log(
+    `hello ${name}, you are ${age} years old, also you are ${gender}`
+  );
+};
+
+sayHello(name, age);
+
+export {};

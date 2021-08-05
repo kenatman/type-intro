97cc33d1ad7d123b8f239fa946f90537359a202c
Yunsu <kenatman0723@gmail.com>
2021-08-05 17:17:20 +0900

    setup for tsc-watch : like nodemon

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sayHello = (name, age, gender) => {
-    return `hello ${name}, you are ${age} years old, also you are ${gender}`;
+    return `hello ${name}, you are ${age} years old, also you are ${gender}!!!`;
 };
 console.log(sayHello(`YS`, 32, `male`));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const sayHello = (name: string, age: number, gender: string): string => {
-  return `hello ${name}, you are ${age} years old, also you are ${gender}`;
+  return `hello ${name}, you are ${age} years old, also you are ${gender}!!!`;
 };
 
 console.log(sayHello(`YS`, 32, `male`));
